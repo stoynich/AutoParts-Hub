833a37923c8153dd634c43f05d59b42501aa24fd
stoynich <kosenkostas495@gmail.com>
2026-03-28 16:25:30 +0300

    feat: реализован VinValidator (проверка формата и существования VIN)

--- AutoParts/src/main/java/autoparts/validation/VinValidator.java
+++ AutoParts/src/main/java/autoparts/validation/VinValidator.java
@@ -3,14 +3,25 @@ package autoparts.validation;
 import autoparts.exception.InvalidVinException;
 
 public class VinValidator {
-    
+
     public void validateVinFormat(String vinCode) throws InvalidVinException {
-        // TODO: занятие 4 - проверить длину 17 символов
-        // TODO: проверить допустимые символы (без I, O, Q)
-        // TODO: бросить InvalidVinException если неверный формат
+
+        if (vinCode == null || vinCode.length() != 17) {
+            throw new InvalidVinException("VIN должен содержать 17 символов", vinCode);
+        }
+
+        if (vinCode.matches(".*[IOQ].*")) {
+            throw new InvalidVinException("VIN содержит запрещённые символы (I, O, Q)", vinCode);
+        }
+
+        if (!vinCode.matches("[A-HJ-NPR-Z0-9]{17}")) {
+            throw new InvalidVinException("VIN содержит недопустимые символы", vinCode);
+        }
     }
-    
+
     public void validateVinExists(String vinCode) throws InvalidVinException {
-        // TODO: занятие 4 - имитация проверки в базе каталога
+        if (vinCode.startsWith("XXX")) {
+            throw new InvalidVinException("VIN не найден в каталоге", vinCode);
+        }
     }
 }
